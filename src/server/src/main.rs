fe0ac81f8d75fa88a98b2f6bb01ad13e7f38ff32
luojia65 <me@luojia.cc>
2018-10-20 17:35:40 +0800

    服务器完工

--- src/server/src/main.rs
+++ src/server/src/main.rs
@@ -1,13 +1,48 @@
 extern crate ws;
+extern crate chrono;
 
 use std::sync::{Arc, Mutex};
 
-fn debug_send(client_addr: ws::Sender, msg: String) {
-    println!("[] {:?} 发送了：{}", client_addr, msg);
+fn hms_string() -> String {
+    use chrono::prelude::*;
+    let now = Local::now();
+    format!("[{}]", now.format("%H:%M:%S"))
 }
 
-fn debug_new_conn(client_addr: ws::Sender) {
-    println!("[] 新的客户端 {:?} 已连接到服务器！", client_addr);
+struct MyHandler {
+    sender: ws::Sender,
+    client_addr: Option<String>,
+    queue: Arc<Mutex<Vec<String>>>,
+}
+
+impl ws::Handler for MyHandler {
+
+    fn on_open(&mut self, shake: ws::Handshake) -> ws::Result<()> {
+        if let Some(client_addr) = shake.remote_addr()? {
+            self.client_addr = Some(client_addr.clone());
+            println!("{} 新的客户端 {:?} 已连接到服务器！", hms_string(), client_addr);
+        }
+        Ok(())
+    }
+
+    fn on_close(&mut self, _code: ws::CloseCode, _reason: &str) {
+        println!("{} 客户端 {:?} 断开了连接！", hms_string(), self.client_addr);
+    }
+
+    fn on_message(&mut self, msg: ws::Message) -> ws::Result<()> { 
+	    if let ws::Message::Text(text) = msg {
+            if let Ok(ref mut mutex) = self.queue.try_lock() {
+                mutex.push(text.clone());
+                println!("{} 客户端 {:?} 发送了信息：{}", hms_string(), self.client_addr, text.clone());
+                self.sender.send(ws::Message::Text(format!("You sent: {}", text)))
+            } else {
+                self.sender.send(ws::Message::Text(format!("Failed to send : {}", text)))
+            }
+	    } else {
+	    	self.sender.close(ws::CloseCode::Unsupported)
+	    }
+     }
+
 }
 
 fn main() {
@@ -17,20 +52,11 @@ fn main() {
     let addr = "0.0.0.0:1103";
     ws::listen(addr, |out| {
         let msg_queue = msg_queue.clone();
-        debug_new_conn(out.clone());
-		move |msg| {
-			if let ws::Message::Text(text) = msg {
-                if let Ok(ref mut mutex) = msg_queue.try_lock() {
-                    mutex.push(text.clone());
-                    debug_send(out.clone(), text.clone());
-                    out.send(ws::Message::Text(format!("You sent: {}", text)))
-                } else {
-                    out.send(ws::Message::Text(format!("Failed to send : {}", text)))
-                }
-			} else {
-				out.close(ws::CloseCode::Unsupported)
-			}
-		}
+        MyHandler {
+            sender: out,
+            client_addr: None,
+            queue: msg_queue
+        }
     }).unwrap();
     println!("Opened receive websocket at {:?}", addr);
     // 2. displayer使用的websocket服务器
