5892ba8df1d79c3a44a13dc3b84fd7cdea428896
luojia65 <me@luojia.cc>
2018-10-20 20:49:24 +0800

    minor edit

--- src/server/src/main.rs
+++ src/server/src/main.rs
@@ -9,42 +9,6 @@ fn hms_string() -> String {
     format!("[{}]", now.format("%H:%M:%S"))
 }
 
-struct MyHandler {
-    sender: ws::Sender,
-    client_addr: Option<String>,
-    queue: Arc<Mutex<Vec<String>>>,
-}
-
-impl ws::Handler for MyHandler {
-
-    fn on_open(&mut self, shake: ws::Handshake) -> ws::Result<()> {
-        if let Some(client_addr) = shake.remote_addr()? {
-            self.client_addr = Some(client_addr.clone());
-            println!("{} 新的客户端 {:?} 已连接到服务器！", hms_string(), client_addr);
-        }
-        Ok(())
-    }
-
-    fn on_close(&mut self, code: ws::CloseCode, _reason: &str) {
-        println!("{} 客户端 {:?} 断开了连接！代码：{:?}", hms_string(), self.client_addr, code);
-    }
-
-    fn on_message(&mut self, msg: ws::Message) -> ws::Result<()> { 
-	    if let ws::Message::Text(text) = msg {
-            if let Ok(ref mut mutex) = self.queue.try_lock() {
-                mutex.push(text.clone());
-                println!("{} {:?} 发送了信息：{}", hms_string(), self.client_addr, text.clone());
-                self.sender.send(ws::Message::Text(format!("You sent: {}", text)))
-            } else {
-                self.sender.send(ws::Message::Text(format!("Failed to send : {}", text)))
-            }
-	    } else {
-	    	self.sender.close(ws::CloseCode::Unsupported)
-	    }
-     }
-
-}
-
 struct MyFactory {
     queue: Arc<Mutex<Vec<String>>>,
 }
@@ -70,6 +34,46 @@ impl ws::Factory for MyFactory {
     }
 }
 
+struct MyHandler {
+    sender: ws::Sender,
+    client_addr: Option<String>,
+    queue: Arc<Mutex<Vec<String>>>,
+}
+
+impl ws::Handler for MyHandler {
+
+    fn on_open(&mut self, shake: ws::Handshake) -> ws::Result<()> {
+        if let Some(client_addr) = shake.remote_addr()? {
+            self.client_addr = Some(client_addr.clone());
+            println!("{} 新的客户端 {} 已连接到服务器！", hms_string(), client_addr);
+        }
+        Ok(())
+    }
+
+    fn on_close(&mut self, code: ws::CloseCode, _reason: &str) {
+        if let Some(client_addr) = self.client_addr.clone() {
+            println!("{} 客户端 {} 断开了连接！代码：{:?}", hms_string(), client_addr, code);
+        }
+    }
+
+    fn on_message(&mut self, msg: ws::Message) -> ws::Result<()> { 
+	    if let ws::Message::Text(text) = msg {
+            if let Ok(ref mut mutex) = self.queue.try_lock() {
+                mutex.push(text.clone());
+                if let Some(client_addr) = self.client_addr.clone() {
+                    println!("{} {} 发送了信息：{}", hms_string(), client_addr, text.clone());
+                }
+                self.sender.send(ws::Message::Text(format!("You sent: {}", text)))
+            } else {
+                self.sender.send(ws::Message::Text(format!("Failed to send : {}", text)))
+            }
+	    } else {
+	    	self.sender.close(ws::CloseCode::Unsupported)
+	    }
+    }
+
+}
+
 fn main() -> ws::Result<()> {
     let msg_queue = Arc::new(Mutex::new(vec![String::new(); 0]));
     // 1. 从客户端输入弹幕
@@ -80,7 +84,7 @@ fn main() -> ws::Result<()> {
         let _socket = ws::WebSocket::new(factory).unwrap()
             .listen(addr.clone()).unwrap();
     });
-    println!("{} DanmuRocket已启动在 {:?}！", hms_string(), addr.clone());
+    println!("{} DanmuRocket已启动在 {}！", hms_string(), addr.clone());
     // 2. displayer使用的websocket服务器
     // 将弹幕数据推送到displayer前端
 
