7320792a5963bec6d5f3d53d457c939cc51de787
luojia65 <me@luojia.cc>
2018-10-29 21:05:17 +0800

    Windows版弹幕发送器。待添加：弹幕颜色修改

---
+++ src/displayer-windows/MainForm.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WebSocketSharp;
+
+namespace DanmuDisplayer
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private WebSocket ws;
+
+        private void ShowMessageForm(string msg) {
+            if (string.IsNullOrWhiteSpace(msg))
+            {
+                return;
+            }
+
+            var form = new MessageForm(msg);
+            form.Show();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+            ws = new WebSocket("ws://live.luojia.cc:11030");
+            ws.OnMessage += (ss, ee) =>
+            {
+                var str = ee.Data;
+                Action<string> action = (s) =>
+                {
+                    LabelState.Text = "Message received! " + s;
+                    ShowMessageForm(s);
+                };
+                Invoke(action, str);
+            };
+            ws.Connect();
+            LabelState.Text = "Connected to server!";
+        }
+
+        private void MainForm_Closing(object sender, EventArgs e)
+        {
+            LabelState.Text = "Disconnected from server!";
+            ws.Close();
+        }
+        
+    }
+}
---
+++ src/displayer-windows/MessageForm.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace DanmuDisplayer
+{
+    partial class MessageForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.LabelMessage = new System.Windows.Forms.Label();
+            this.Ticker = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // LabelMessage
+            // 
+            this.LabelMessage.AutoSize = true;
+            this.LabelMessage.Font = new System.Drawing.Font("黑体", 25.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.LabelMessage.ForeColor = System.Drawing.Color.White;
+            this.LabelMessage.Location = new System.Drawing.Point(12, 9);
+            this.LabelMessage.Name = "LabelMessage";
+            this.LabelMessage.Size = new System.Drawing.Size(310, 69);
+            this.LabelMessage.TabIndex = 2;
+            this.LabelMessage.Text = "测试弹幕";
+            // 
+            // Ticker
+            // 
+            this.Ticker.Enabled = true;
+            this.Ticker.Interval = 10;
+            this.Ticker.Tick += new System.EventHandler(this.Ticker_Tick);
+            // 
+            // MainForm
+            // 
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(233)))), ((int)(((byte)(233)))), ((int)(((byte)(233)))));
+            this.ClientSize = new System.Drawing.Size(1020, 622);
+            this.Controls.Add(this.LabelMessage);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "MainForm";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label LabelMessage;
+        private System.Windows.Forms.Timer Ticker;
+    }
+}
+
